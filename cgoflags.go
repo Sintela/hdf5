db54985b2f027e1ce87d857afc8ae1a685d5f3cc
Connor Timmins <connor.timmins@sintela.com>
2024-02-29 16:44:47 +0000

    Fix link order on x86

--- cgoflags.go
+++ cgoflags.go
@@ -7,7 +7,7 @@ package hdf5
 // #cgo LDFLAGS: -L/usr/local/lib -L${SRCDIR}/lib
 // #cgo CFLAGS: -I${SRCDIR}/include -I/usr/local/include
 // #cgo darwin LDFLAGS: -lhdf5_darwin -lhdf5_hl_darwin -lsz_darwin
-// #cgo linux,!arm64 LDFLAGS: -L/lib/x86_64-linux-gnu -lhdf5_x86 -lhdf5_hl_x86 -lsz_x86 -lz
+// #cgo linux,!arm64 LDFLAGS: -L/lib/x86_64-linux-gnu  -lhdf5_hl_x86 -lhdf5_x86 -lsz_x86 -lz
 // #cgo linux,arm64 LDFLAGS: -L/usr/lib/aarch64-linux-gnu  -lhdf5_hl_arm64 -lhdf5_arm64 -lsz_arm64 -lz
 // #include "hdf5.h"
 // #include "hdf5_hl.h"
