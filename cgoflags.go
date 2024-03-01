3855aa0f5171d938295abe9854305b27a8ca3bca
Connor Timmins <connor.timmins@sintela.com>
2024-03-01 14:36:06 +0000

    More flags

--- cgoflags.go
+++ cgoflags.go
@@ -6,9 +6,9 @@ package hdf5
 
 // #cgo LDFLAGS: -L/usr/local/lib -L${SRCDIR}/lib
 // #cgo CFLAGS: -I${SRCDIR}/include -I/usr/local/include
-// #cgo darwin,!arm64 LDFLAGS: -lhdf5_darwin_x86 -lhdf5_hl_darwin_x86 -lsz_darwin_x86
+// #cgo darwin,!arm64 LDFLAGS: -lhdf5_darwin_x86 -lhdf5_hl_darwin_x86 -lsz_darwin_x86 -lz -lm -ldl
 // #cgo darwin,!arm64 CFLAGS: -I/usr/local/Cellar/gcc/13.2.0/include/c++/13/paralle
-// #cgo darwin,arm64 LDFLAGS: -lhdf5_darwin_arm64 -lhdf5_hl_darwin_arm64 -lsz_darwin_arm64
+// #cgo darwin,arm64 LDFLAGS: -lhdf5_darwin_arm64 -lhdf5_hl_darwin_arm64 -lsz_darwin_arm64 -lz -lm -ldl
 // #cgo darwin,arm64 CFLAGS: -I/opt/homebrew/Cellar/gcc/13.2.0/include/c++/13/parallel
 // #cgo linux,!arm64 LDFLAGS: -L/lib/x86_64-linux-gnu  -lhdf5_hl_x86 -lhdf5_x86 -lsz_x86 -lz -lm -ldl
 // #cgo linux,arm64 LDFLAGS: -L/usr/lib/aarch64-linux-gnu  -lhdf5_hl_arm64 -lhdf5_arm64 -lsz_arm64 -lz -lm -ldl
