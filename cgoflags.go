7651ebacd38c02bd0736fadead9f7d0029af8262
Connor Timmins <connor.timmins@sintela.com>
2024-03-01 13:40:30 +0000

    Add Darwin arm64 libs

--- cgoflags.go
+++ cgoflags.go
@@ -6,7 +6,8 @@ package hdf5
 
 // #cgo LDFLAGS: -L/usr/local/lib -L${SRCDIR}/lib
 // #cgo CFLAGS: -I${SRCDIR}/include -I/usr/local/include
-// #cgo darwin LDFLAGS: -lhdf5_darwin -lhdf5_hl_darwin -lsz_darwin
+// #cgo darwin,!arm64 LDFLAGS: -lhdf5_darwin_x86 -lhdf5_hl_darwin_x86 -lsz_darwin_x86
+// #cgo darwin,arm64 LDFLAGS: -lhdf5_darwin_arm64 -lhdf5_hl_darwin_arm64 -lsz_darwin_arm64
 // #cgo linux,!arm64 LDFLAGS: -L/lib/x86_64-linux-gnu  -lhdf5_hl_x86 -lhdf5_x86 -lsz_x86 -lz -lm -ldl
 // #cgo linux,arm64 LDFLAGS: -L/usr/lib/aarch64-linux-gnu  -lhdf5_hl_arm64 -lhdf5_arm64 -lsz_arm64 -lz -lm -ldl
 // #include "hdf5.h"
